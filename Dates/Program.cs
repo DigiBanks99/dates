5e91c10d88b5f3b53e4b65bbd6427a17bb796989
Wilco Boshoff <wilco.boshoff@entelect.co.za>
2018-07-27 13:53:31 +0200

    Eastern Standard Time

--- Dates/Program.cs
+++ Dates/Program.cs
@@ -6,9 +6,22 @@ namespace Dates
   {
     static void Main(string[] args)
     {
-      DateTime utc = DateTime.UtcNow;
+      try
+      {
+        var utc = DateTime.UtcNow;
 
-      Console.WriteLine($"{utc:o}\tKind: {utc.Kind}");
+        Console.WriteLine($"{utc:o}\tKind: {utc.Kind}");
+
+        var tz = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+
+        var est = TimeZoneInfo.ConvertTimeFromUtc(utc, tz);
+
+        Console.WriteLine($"{est:o}\tKind: {est.Kind}");
+      }
+      catch (Exception ex)
+      {
+        Console.Error.Write(ex);
+      }
     }
   }
 }
